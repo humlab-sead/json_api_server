ec3482a7100aa30833cc483508e9049f1f333443
Johan von Boer <johan.von.boer@umu.se>
2022-10-31 16:13:20 +0100

    added support for c14 std dating method (method 151)

--- src/Modules/DatingModule.class.js
+++ src/Modules/DatingModule.class.js
@@ -1,7 +1,9 @@
+const fs = require('fs');
+
 class DatingModule {
     constructor(app) {
         this.name = "Dating";
-        this.moduleMethodGroups = [19, 20];
+        this.moduleMethodGroups = [19, 20, 151]; //should maybe include 21 as well...
         this.app = app;
         this.expressApp = this.app.expressApp;
     }
@@ -60,14 +62,73 @@ class DatingModule {
         WHERE analysis_entity_id=$1;
         `;
 
+        let c14stdSql = `
+        SELECT 
+        tbl_analysis_entities.*,
+        tbl_geochronology.*,
+        tbl_dating_uncertainty.description AS dating_uncertainty_desc,
+        tbl_dating_uncertainty.uncertainty AS dating_uncertainty
+        FROM tbl_analysis_entities
+        JOIN tbl_geochronology ON tbl_geochronology.analysis_entity_id = tbl_analysis_entities.analysis_entity_id
+        LEFT JOIN tbl_dating_uncertainty ON tbl_dating_uncertainty.dating_uncertainty_id = tbl_geochronology.dating_uncertainty_id
+        WHERE tbl_analysis_entities.analysis_entity_id=$1;
+        `;
+        
+
         let queryPromises = [];
+
         site.sample_groups.forEach(sampleGroup => {
             sampleGroup.physical_samples.forEach(physicalSample => {
                 physicalSample.analysis_entities.forEach(analysisEntity => {
-                    let promise = pgClient.query(sql, [analysisEntity.analysis_entity_id]).then(values => {
-                        analysisEntity.dating_values = values.rows[0];
-                    });
+
+                    //Here we need to check what dataset this AE is linked to
+                    //if it is a dataset with method_id 151 then it's 'C14 std'
+                    //which needs to be handled/fetched differently from the other dating methods
+
+                    let specialTreatment = false;
+                    for(let key in site.datasets) {
+                        if(analysisEntity.dataset_id == site.datasets[key].dataset_id) {
+                            if(site.datasets[key].method_id == 151) {
+                                //This needs special treatment
+                                specialTreatment = true;
+                            }
+                        }
+                    }
+
+                    let promise = null;
+                    if(!specialTreatment) {
+                        promise = pgClient.query(sql, [analysisEntity.analysis_entity_id]).then(values => {
+                            analysisEntity.dating_values = values.rows[0];
+                        });
+                    }
+                    else {
+                        promise = pgClient.query(c14stdSql, [analysisEntity.analysis_entity_id]).then(values => {
+                            let r = values.rows[0];
+                            analysisEntity.dating_values = {
+                                "geochron_id": r.geochron_id,
+                                "dating_lab_id": r.dating_lab_id,
+                                "lab_number": r.lab_number,
+                                "age": r.age,
+                                "error_older": r.error_older,
+                                "error_younger": r.error_younger,
+                                "delta_13c": r.delta_13c,
+                                "notes": r.notes,
+                                "dating_uncertainty_id": r.dating_uncertainty_id,
+                                "dating_uncertainty": r.dating_uncertainty,
+                                "dating_uncertainty_desc": r.dating_uncertainty_desc
+                            };
+
+                            //if dating_lab_id seems to be a number and we don't already have his lab 
+                            //among the lookups, add it
+                            const datingLabId = parseInt(r.dating_lab_id);
+                            if(datingLabId != NaN) {
+                                let labFetchPromise = this.fetchDatingLab(site, datingLabId);
+                                queryPromises.push(labFetchPromise);
+                            }
+                        });
+                    }
                     queryPromises.push(promise);
+                    
                 });
             })
         });
@@ -77,6 +138,31 @@ class DatingModule {
         });
 
         return site;
+    }
+
+    async fetchDatingLab(site, datingLabId) {
+        let pgClient = await this.app.getDbConnection();
+        if(!pgClient) {
+            return false;
+        }
+        
+        if(typeof site.lookup_tables.labs == "undefined") {
+            site.lookup_tables.labs = [];
+        }
+
+        return pgClient.query("SELECT * FROM tbl_dating_labs WHERE dating_lab_id=$1", [datingLabId]).then(values => {
+            let labMeta = values.rows[0];
+            let foundLab = false;
+            site.lookup_tables.labs.forEach(lab => {
+                if(lab.dating_lab_id == datingLabId) {
+                    foundLab = true;
+                }
+            });
+            if(!foundLab) {
+                site.lookup_tables.labs.push(labMeta);
+            }
+            this.app.releaseDbConnection(pgClient);
+        });
     }
 
     getDataGroupByMethod(dataGroups, methodId) {
