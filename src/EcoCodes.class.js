542f257425aaa32f160f3e4f097f204a41e868ad
Johan von Boer <johan.von.boer@umu.se>
2023-01-20 15:21:27 +0100

    just changed out some old functions

--- src/EcoCodes.class.js
+++ src/EcoCodes.class.js
@@ -160,7 +160,7 @@ class EcoCodes {
             }
         }
 
-        let site = await this.app.getSiteFromCache(siteId);
+        let site = await this.app.getObjectFromCache("sites", { site_id: siteId });
         if(!site) {
             //If we don't have this site cached we give up on this request (at the moment!), but should perhaps be handled better in the future
             console.warn("Site "+siteId+" is not cached");
@@ -280,7 +280,7 @@ class EcoCodes {
             }
         }
 
-        let site = await this.app.getSiteFromCache(siteId);
+        let site = await this.getObjectFromCache("sites", { site_id: siteId });
         if(!site) {
             //If we don't have this site cached we give up on this request (at the moment!), but should perhaps be handled better in the future
             console.warn("Site "+siteId+" is not cached");
