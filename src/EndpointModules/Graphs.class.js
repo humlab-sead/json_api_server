b9ccdcf77a53d78766066c026a18260dd0fced36
Johan von Boer <johan.von.boer@umu.se>
2024-03-08 16:09:35 +0100

    some experimental work with adding graph data to the result table

--- src/EndpointModules/Graphs.class.js
+++ src/EndpointModules/Graphs.class.js
@@ -4,25 +4,46 @@ class Graphs {
     constructor(app) {
         this.app = app;
 
-        this.app.expressApp.post('/graphs/analysis_methods', async (req, res) => {
-            let siteIds = req.body;
-            if(typeof siteIds != "object") {
+        //add a graph endpoint for rendering an overview of domain data for a site
+        this.app.expressApp.post('/graphs/site/domains_overview', async (req, res) => {
+          let siteIds = req.body;
+          if(typeof siteIds != "object") {
+              res.status(400);
+              res.send("Bad input - should be an array of site IDs");
+              return;
+          }
+          siteIds.forEach(siteId => {
+            if(!parseInt(siteId)) {
                 res.status(400);
                 res.send("Bad input - should be an array of site IDs");
                 return;
             }
-            
-            siteIds.forEach(siteId => {
-                if(!parseInt(siteId)) {
-                    res.status(400);
-                    res.send("Bad input - should be an array of site IDs");
-                    return;
-                }
-            });
+        });
 
-            let analysisMethods = await this.fetchAnalysisMethodsSummaryForSites(siteIds);
-            res.header("Content-type", "application/json");
-            res.end(JSON.stringify(analysisMethods, null, 2));
+          let data = await this.fetchDomainsOverviewForSites(siteIds);
+          res.header("Content-type", "application/json");
+          res.end(JSON.stringify(data, null, 2));
+        });
+
+        this.app.expressApp.post('/graphs/analysis_methods', async (req, res) => {
+          let siteIds = req.body;
+          if(typeof siteIds != "object") {
+              res.status(400);
+              res.send("Bad input - should be an array of site IDs");
+              return;
+          }
+          
+          siteIds.forEach(siteId => {
+              if(!parseInt(siteId)) {
+                  res.status(400);
+                  res.send("Bad input - should be an array of site IDs");
+                  return;
+              }
+          });
+
+          let analysisMethods = await this.fetchAnalysisMethodsSummaryForSites(siteIds);
+          res.header("Content-type", "application/json");
+          res.end(JSON.stringify(analysisMethods, null, 2));
         });
 
         this.app.expressApp.post('/graphs/feature_types', async (req, res) => {
@@ -289,14 +310,78 @@ class Graphs {
         return resultObject;
     }
 
-    async fetchAnalysisMethodsSummaryForSites(siteIds) {
+    async fetchDatasetOverviewForSites(siteIds) {
+        
+    }
+
+    //WARNING: this function does seem to work, but not all datasets are covered by domains (such as dating methods), so the result is less useful than I would like since it doesn't do a very good job of showing an overview of what data is available in a site
+    async fetchDomainsOverviewForSites(siteIds) {
         let cacheId = crypto.createHash('sha256');
-        cacheId = cacheId.update('analysismethods'+JSON.stringify(siteIds)).digest('hex');
+        cacheId = cacheId.update('domains'+JSON.stringify(siteIds)).digest('hex');
         let identifierObject = { cache_id: cacheId };
       
         let cachedData = await this.app.getObjectFromCache("graph_cache", identifierObject);
         if (cachedData !== false) {
           return cachedData;
+        }
+
+        //let's compile a summary of the domains covered by this site and how many analysis_entities are in each domain
+        //we do this by matching $datasets.method_id with the method_ids in the lookup_tables.domains
+      
+        let pipeline = [
+          { $match: { site_id: { $in: siteIds } } },
+          { $unwind: "$datasets" },
+          { $unwind: "$lookup_tables.domains" },
+          {
+            $match: {
+              "$expr": {
+                "$in": ["$datasets.method_id", "$lookup_tables.domains.method_ids"]
+              }
+            }
+          },
+          {
+            $group: {
+              _id: "$lookup_tables.domains.facet_code",
+              dataset_count: { $sum: 1 },
+              domain_meta: { $first: "$lookup_tables.domains" }
+            }
+          },
+          {
+            $project: {
+              method_id: "$_id",
+              _id: 0,
+              dataset_count: 1,
+              domain_meta: 1
+            }
+          }
+        ];
+        
+        let sites = await this.app.mongo.collection('sites').aggregate(pipeline).toArray();
+
+        let domains = sites.map(site => ({
+          ...site.domain_meta,
+          dataset_count: site.dataset_count
+        }));
+      
+        let resultObject = {
+          cache_id: cacheId,
+          domains: domains
+        };
+      
+        this.app.saveObjectToCache("graph_cache", identifierObject, resultObject);
+        return resultObject;
+    }
+
+    async fetchAnalysisMethodsSummaryForSites(siteIds) {
+        let cacheId = crypto.createHash('sha256');
+        cacheId = cacheId.update('analysismethods'+JSON.stringify(siteIds)).digest('hex');
+        let identifierObject = { cache_id: cacheId };
+      
+        if(this.app.useGraphCaching) {
+          let cachedData = await this.app.getObjectFromCache("graph_cache", identifierObject);
+          if (cachedData !== false) {
+            return cachedData;
+          }
         }
       
         let pipeline = [
@@ -338,7 +423,9 @@ class Graphs {
           analysis_methods_datasets: methods
         };
       
-        this.app.saveObjectToCache("graph_cache", identifierObject, resultObject);
+        if(this.app.useGraphCaching) {
+          this.app.saveObjectToCache("graph_cache", identifierObject, resultObject);
+        }
       
         return resultObject;
     }
